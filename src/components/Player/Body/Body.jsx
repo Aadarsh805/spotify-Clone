c8464a440bbd337dba221beade6878ed83ba6e61
Aadarsh Thakur <aadarshthakur80501@gmail.com>
2022-07-16 17:14:21 +0530

    changed banner

--- src/components/Player/Body/Body.jsx
+++ src/components/Player/Body/Body.jsx
@@ -15,8 +15,8 @@ const Body = ({ spotify }) => {
       <div className="body__info">
         <img src={discover_weekly?.images[0].url} alt="" />
         <div className="body__infoText">
-          <strong>PLAYLIST</strong>
-          <h2>New</h2>
+          <strong>Top 50</strong>
+          <h2>Global</h2>
           <p>{discover_weekly?.description}</p>
         </div>
       </div>
