347a52881963d683f81184c48a440b606fa80141
Aadarsh Thakur <aadarshthakur80501@gmail.com>
2022-07-17 00:44:11 +0530

    changes

--- src/components/Player/Body/Body.jsx
+++ src/components/Player/Body/Body.jsx
@@ -6,18 +6,21 @@ import PlayCircleFilledIcon from "@mui/icons-material/PlayCircleFilled";
 import FavoriteIcon from "@mui/icons-material/Favorite";
 import MoreHorizIcon from "@mui/icons-material/MoreHoriz";
 import SongRow from "./SongRow/SongRow";
+import AccessTimeIcon from '@mui/icons-material/AccessTime';
 
 const Body = ({ spotify }) => {
   const [{ discover_weekly }, dispatch] = useStateValue();
   return (
     <div className="body">
-      <Header spotify={spotify} />
       <div className="body__info">
+      <Header spotify={spotify} />
+        <div className="body__global">
         <img src={discover_weekly?.images[0].url} alt="" />
         <div className="body__infoText">
-          <strong>Top 50</strong>
-          <h2>Global</h2>
+          <strong>Playlist</strong>
+          <h2>Top 50-Global</h2>
           <p>{discover_weekly?.description}</p>
+        </div>
         </div>
       </div>
 
@@ -27,6 +30,14 @@ const Body = ({ spotify }) => {
           <FavoriteIcon fontSize="large" />
           <MoreHorizIcon />
         </div>
+
+
+        <div className="heading__row">
+          <span>title</span>
+          <span>album</span>
+          <AccessTimeIcon fontsize="small"/>
+        </div>
+        <hr />
 
         {discover_weekly?.tracks?.items.map((item) => (
           <SongRow track={item.track} />
--- src/reducer.js
+++ src/reducer.js
@@ -1,36 +1,36 @@
 export const initialState = {
-    user: null, 
-    playlists: [],
-    playing: false,
-    item: null,
-    // token:"BQBagM1rRpDe367duE4oCrY_7TPFvgu3AevSHnehVLJoGm3wc_QmOkTPDUGvQB-uz6_EvZU0qJCLXOdWXLeWjD96Fc4Op6VSxVnxWzt5byNtmCcJRo1smLRLsf0WAPgCsT6XHXbvAca848eZdz3ae3MqqZ8q1tx_VVkzUy4ggPjS8k-TrbbglN55boplnYa8v48HJ0JGNQoboJfDprXJ",
-}
+  user: null,
+  playlists: [],
+  playing: false,
+  item: null,
+  // token:"BQBagM1rRpDe367duE4oCrY_7TPFvgu3AevSHnehVLJoGm3wc_QmOkTPDUGvQB-uz6_EvZU0qJCLXOdWXLeWjD96Fc4Op6VSxVnxWzt5byNtmCcJRo1smLRLsf0WAPgCsT6XHXbvAca848eZdz3ae3MqqZ8q1tx_VVkzUy4ggPjS8k-TrbbglN55boplnYa8v48HJ0JGNQoboJfDprXJ",
+};
 
 const reducer = (state, action) => {
-    switch(action.type){
-        case 'SET_USER':
-            return {
-                ...state,
-                user: action.user
-            }
-        case 'SET_TOKEN':
-            return {
-                ...state,
-                token: action.token
-            }
-        case 'SET_PLAYLISTS':
-            return {
-                ...state,
-                playlists: action.playlists
-            }
-        case 'SET_DISCOVER_WEEKLY':
-            return {
-                ...state,
-                discover_weekly: action.discover_weekly,
-            }
-        default: 
-            return state;   
-    }
-}
+  switch (action.type) {
+    case "SET_USER":
+      return {
+        ...state,
+        user: action.user,
+      };
+    case "SET_TOKEN":
+      return {
+        ...state,
+        token: action.token,
+      };
+    case "SET_PLAYLISTS":
+      return {
+        ...state,
+        playlists: action.playlists,
+      };
+    case "SET_DISCOVER_WEEKLY":
+      return {
+        ...state,
+        discover_weekly: action.discover_weekly,
+      };
+    default:
+      return state;
+  }
+};
 
 export default reducer;
